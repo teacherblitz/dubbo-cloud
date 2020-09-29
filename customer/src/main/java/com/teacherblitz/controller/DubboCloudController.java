05dba31f033658a756611ac6a7b7b456e202eb60
zhuyuan <zhuyuan@yinzetong.com>
2020-09-29 18:46:08 +0800

    nacos配置自动刷新

--- customer/src/main/java/com/teacherblitz/controller/DubboCloudController.java
+++ customer/src/main/java/com/teacherblitz/controller/DubboCloudController.java
@@ -4,6 +4,7 @@ import com.teacherblitz.service.DubboCloudService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @since: 2020/8/14
  */
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("/dubbo")
 public class DubboCloudController {
 
-    @Value("${dubbo.application.name}")
+    @Value("${sleep}")
     private String serviceName;
 
     @DubboReference(version = "1.0.0")
@@ -30,6 +32,7 @@ public class DubboCloudController {
      */
     @RequestMapping("/")
     public String dayHello() {
+        log.info("当前服务：{}", serviceName);
         String name = dubboCloudService.sayHello("teacherblitz");
         log.info("【服务消费者】：接收到消息={}", name);
         return name;
